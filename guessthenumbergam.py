d7cd737e6cf970a0169a8369f3debf3d95643407
r0b3rt210 <robytam349@gmail.com>
2026-06-15 16:27:14 +0100

    Add files via upload

--- guessthenumbergam.py
+++ guessthenumbergam.py
@@ -1,20 +1,30 @@
 import tkinter
 import random
+import tkinter.messagebox
 screen = tkinter.Tk()
 screen.geometry("500x500")
 screen.title("guess the number")
 number = random.randint(1 , 100)
+playername = ""
 def guess1():
-    if number >= 50:
-        print("the number is above 49")
-    if number <=50:
-        print("the number is less than 51")
+    playerguess = int(urguess.get())
+    if number > playerguess:
+        tkinter.messagebox.showwarning("guess" , "your number is lower")
+  
+    elif number < playerguess:
+        tkinter.messagebox.showwarning("guess" , "your number is higher")
+    elif number == playerguess:
+        tkinter.messagebox.showinfo("guess" , "your guess is correct ")
+
+def namee():
+    playername = entername.get()
+    tkinter.messagebox.showinfo("greeting" , f"hello {playername}, guess the number between 1-100")
 name = tkinter.Label(screen , text = "what is your name?")
 entername=tkinter.Entry(screen )
-ok = tkinter.Button(screen , text = "Ok")
-takeaguess = tkinter.Label(screen , text = "take a guess")
+ok = tkinter.Button(screen , text = "Ok" , command=namee)
+takeaguess = tkinter.Label(screen , text = f"take a guess,{playername}")
 urguess=tkinter.Entry(screen )
-guess = tkinter.Button(screen , text = "guess")
+guess = tkinter.Button(screen , text = "guess" , command=guess1)
 name.grid(row=2 , column=1)
 entername.grid(row=2 , column=2)
 ok.grid(row=2 , column=3)
